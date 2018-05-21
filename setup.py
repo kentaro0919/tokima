f447a5f8ba7be60b8c92e4c5dda19ca5aa452eee
Kentaro0919 <kentaro0919@gmail.com>
2018-05-21 14:24:40 +0900

    test to upload

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup  # , find_packages
 
-long_description = """Tools for my own use."""
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='tokima', version='0.0.1', description='A Utility Tools', long_description=long_description,)
-
+setup(
+    name='tokima',
+    version='0.0.1',
+    author="Kentaro Matsuzaki",
+    author_email="kentaro0919@gmail.com",
+    description='A Utility Tools',
+    long_description=long_description,
+    url="https://github.com/kentaro0919/tokima",
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.6',
+                 ],
+    )
